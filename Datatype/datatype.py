7ec235d3de4e29e982481b0649dd2cbb2a8f65d7
Md sowrov sarkar <59117363+sowrovsarkar63@users.noreply.github.com>
2021-04-06 14:03:30 +0600

    Data type

--- Datatype/datatype.py
+++ Datatype/datatype.py
@@ -1,1 +1,36 @@
-print("Data type in python")
+# There are many data type...
+# Let's check them out .....
+
+
+
+
+# String Data types:
+
+stringDatatype = "This is string"
+
+
+if isinstance(stringDatatype, str):
+    print(stringDatatype)
+else:
+  print("It's not string")
+  
+  
+# Integers
+Integer = 10
+if isinstance(Integer, int):
+    print("this is integer")
+    
+    # Bolean
+    
+    boool = True
+    
+    # Float
+    Float = 3.10
+    # complex
+    complexx = 3j;
+    print(type(complexx))
+    
+
+ 
+  
+
