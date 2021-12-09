28c7531babab32802f1ba2580a5b881956d413c8
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-12-09 18:34:07 +0600

    linkedlist

--- datastructure-js/linkedList.js
+++ datastructure-js/linkedList.js
@@ -1,1 +1,43 @@
-console.log("This is from linkedList File ");
+console.log("Everything is working ");
+console.log("-----------------------------------");
+console.log("==============================");
+
+// const list = {
+//     head:{
+//         value:6
+//         next:{
+//             vlaue:10
+//             next:{
+//                 value:12
+//                 next:{
+//                     value:3
+//                     next:null
+//                 }
+//             }
+//         }
+//     }
+// }
+// console.log(list);
+//
+class ListNode {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// Implementing LinkeList In JavaScript ..
+
+class LinkeList {
+    constructor(head = null) {
+        this.head = head;
+    }
+}
+
+let node1 = new ListNode(2);
+let node2 = new ListNode(5);
+node1.next = node2;
+
+let list = new LinkeList(node1);
+
+console.log(list.head.next.data);
