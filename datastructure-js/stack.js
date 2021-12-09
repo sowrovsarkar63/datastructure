01528cc4b3c47e753c3cc5ac592721276acb1e1a
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-12-09 23:59:21 +0600

    Stack Data Structure Minimal Implementation In JavaScript

--- datastructure-js/stack.js
+++ datastructure-js/stack.js
@@ -1,1 +1,37 @@
-console.log("Class DataStructure");
+console.log("-----------------------");
+class Stack {
+    constructor() {
+        this.stack = [];
+    }
+    add(elements) {
+        return this.stack.push(elements);
+    }
+
+    remove(elements) {
+        return this.stack.pop();
+    }
+}
+
+let StackDsa = new Stack();
+
+StackDsa.add("html");
+console.log(StackDsa.stack);
+StackDsa.add("css");
+console.log(StackDsa.stack);
+StackDsa.add("js");
+console.log(StackDsa.stack);
+StackDsa.add("react");
+console.log(StackDsa.stack);
+StackDsa.add("material ui");
+
+console.log(StackDsa.stack);
+// Remove Data From Array
+
+StackDsa.remove();
+console.log(StackDsa.stack);
+StackDsa.remove();
+console.log(StackDsa.stack);
+StackDsa.remove();
+console.log(StackDsa.stack);
+StackDsa.remove();
+console.log(StackDsa.stack);
